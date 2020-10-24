1fdf1d22799260058da98f0df496c9b8ff279abe
Geovanny Brian Encinas Boza <geoenci@gmail.com>
2020-10-24 13:18:22 -0400

    Agregado de la funcionalidad de ordenamiento y listado de paginacion

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: 'user',
-    password: '123456',
+    user: 'postgres',
+    password: 'zsecsq159357',
     host: 'localhost',
     port: 5432,
     database: 'gestion_productos',
--- src/modules/products/model.js
+++ src/modules/products/model.js
@@ -1,11 +1,52 @@
 const { response } = require('express')
 const pool = require('../../database')
 
-async function getProduct(){
-    const response = await pool.query(
-        'SELECT * FROM products;'
+async function getProduct(criterio,page,limit){
+
+    if(criterio == ''){
+        const response = await pool.query(
+            "SELECT * FROM producto ORDER BY cod_prod;"
+        )
+        var result1 = response.rows
+        
+    }else{
+        const response = await pool.query(
+            "SELECT * FROM producto ORDER BY "+criterio+";"
+        )
+        
+        var result1 = response.rows
+          
+    }
+    
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const results = {}
+    
+
+    results.results = result1.slice(startIndex,endIndex)
+
+    if(endIndex < result1.length ){
+        results.next = {
+            page: page + 1,
+            limit : limit
+        }
+    }
+    if(startIndex > 0){
+        results.previus = {
+            page: page - 1,
+            limit : limit
+       }
+    }
+
+    const ros = await pool.query(
+        "SELECT count(*) FROM producto;"
     )
-    return response.rows
+    
+    results.cant = ros.rows
+
+
+
+    return results
 }
 
 async function getProductById(id){
