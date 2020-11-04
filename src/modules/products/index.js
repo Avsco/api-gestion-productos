df8052c7d6c944c3ab769328eda46e293c14ecf4
Victor E. Abasto Pérez <victor.abasto.p@gmail.com>
2020-11-03 20:40:21 -0400

    Reparación de errores

--- src/modules/products/index.js
+++ src/modules/products/index.js
@@ -4,7 +4,7 @@ const router = Router()
 const { GET, SHOW, POST, PUT, DELETE, GETIMAGE } = require('./controllers')
 
 router.get('/products', GET)
-router.get('/products/:id/:cantidad', SHOW)
+router.get('/products/:id', SHOW)
 router.post('/products', POST)
 router.put('/products/:id', PUT)
 router.delete('/products/:id', DELETE)
--- src/modules/products/model.js
+++ src/modules/products/model.js
@@ -97,11 +97,7 @@ async function categoryManage(categoria){
 
 async function createProduct(nombre_prod, descripcion, categoria, precio_unid, cantidad, peso, unidad_med, fecha_venc){
     const response =  await pool.query(
-    /*'INSERT INTO producto (cod_cat, nombre_prod, descripcion, precio_unid, cantidad, peso, unidad_med, fecha_venc, fecha_adic) 
-    SELECT c.cod_cat, $1, $2, $4, $5, $6, $7, $8, CURRENT_DATE FROM categoria c, producto p WHERE 
-    c.nombre_cat=$3 AND c.cod_cat=p.cod_cat;',*/
-    /*Arriba está la consulta sql que se usaba antes, pero el trigger no detectaba errrores, se hizo un cambio a la versión de
-    abajo*/
+    
     `insert into producto (cod_cat, nombre_prod, descripcion, precio_unid, cantidad, peso, unidad_med, fecha_venc,fecha_adic)
         values ((select cod_cat
         from categoria
