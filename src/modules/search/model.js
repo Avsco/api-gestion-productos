5d5ace4f97c7e4e0762c1b7730096d3648d510e0
Avsco <benacho82@gmail.com>
2020-11-16 07:50:07 -0400

    add: query for discounts

--- src/modules/search/model.js
+++ src/modules/search/model.js
@@ -20,18 +20,36 @@ async function search(expresion, table, page, limit){
 }
 
 async function searchProducts(expresion, page, limit){
-   expresion = expresion.toLowerCase()
+    expresion = expresion.toLowerCase()
     page = (page - 1) * limit
     const response = await pool.query(
-        `SELECT nombre_prod 
+        `SELECT * 
         FROM producto 
         WHERE LOWER(nombre_prod) LIKE '%${expresion}%' 
-        AND (fecha_venc > NOW() or fecha_venc IS NULL)
+        AND (fecha_venc > NOW() OR fecha_venc IS NULL)
         LIMIT $1 OFFSET $2;`,
         [limit, page]
     )
     return response.rows
 }
+
+async function searchDiscounts(expresion, page, limit){
+    expresion = expresion.toLowerCase()
+    page = (page - 1) * limit
+    const response = await pool.query(
+        `SELECT *
+        FROM descuento, (SELECT cod_prod, nombre_prod, descripcion, precio_unid, fecha_adic
+            FROM producto 
+            WHERE LOWER(nombre_prod) LIKE '%${expresion}%'
+            AND (fecha_venc > NOW() OR fecha_venc IS NULL)) as UNO
+        WHERE UNO.cod_prod = descuento.cod_prod
+        LIMIT $1 OFFSET $2;`,
+        [limit, page]
+    )
+    return response.rows
+}
+
+async function searchPromotions(expresion, page, limit){}
 
 async function countRows(nameTable) {
     const response = await pool.query(
@@ -39,10 +57,6 @@ async function countRows(nameTable) {
     )
     return response.rows
 }
-
-async function searchDiscounts(expresion, page, limit){}
-
-async function searchPromotions(expresion, page, limit){}
 
 module.exports = { 
     search
